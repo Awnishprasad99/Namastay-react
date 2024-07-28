0099356f1d59f9dc7f4d6dc556ff07008abb6aec
Awnish prasad sharma <awnishprasad99@gmail.com>
2024-07-28 17:08:56 +0530

    new commit

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
+import React from 'react'
 
+const app = () => {
+  return (
+    <div>
+      <h1>hello workd</h1>
+    </div>
+  )
+}
 
-
-
-
-
-
-
-
-
-const parent = React.createElement("div",{id:parent},[React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I am h1"),
-    React.createElement("h2",{},"i am h2")
-])
-,React.createElement("div",{id:"child2"},[
-    React.createElement("h1",{},"I am h1"),
-    React.createElement("h2",{},"i ma h2 tag")
-])
-
-
-
-]);
-
-console.log(parent)
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-root.render(parent)
+export default app;
